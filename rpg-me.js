6374933fc60e431e8746ba9d5e51d776c36d373a
LigerCal-13 <clc6449@psu.edu>
2024-12-17 16:59:50 -0500

    test commit

--- rpg-me.js
+++ rpg-me.js
@@ -5,6 +5,8 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "@haxtheweb/rpg-character/rpg-character.js";
+import "wired-elements";
 
 /**
  * `rpg-me`
